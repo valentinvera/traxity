1ff0009cf8e05e5cf68aab9bf8ac5ab3daa398ba
Valentín Vera <valentinvera2805@gmail.com>
2025-04-18 00:07:09 -0300

    feat: update PrismaClient import path

--- apps/api/src/prisma/prisma.service.ts
+++ apps/api/src/prisma/prisma.service.ts
@@ -1,5 +1,5 @@
+import { PrismaClient } from "@/generated/prisma/client"
 import { Injectable, OnModuleDestroy, OnModuleInit } from "@nestjs/common"
-import { PrismaClient } from "@prisma/client"
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
